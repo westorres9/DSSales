c86d6d72a841334af68dfa27775657e7ba1b8d29
westorres9 <wester.torres9@gmail.com>
2022-07-04 09:02:16 -0300

    ManyToMany User Role

--- backend/src/main/java/com/devsuperior/dssales/User.java
+++ backend/src/main/java/com/devsuperior/dssales/User.java
@@ -2,7 +2,10 @@ package com.devsuperior.dssales;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
+
 @Entity
 @Table(name = "tb_user")
 public class User implements Serializable {
@@ -17,6 +20,11 @@ public class User implements Serializable {
     private String email;
     private String password;
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "tb_user_role",
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
     public User(){
     }
 
@@ -57,6 +65,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
     }
 
     @Override
